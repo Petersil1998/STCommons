fdf1c2618d7aa3f3fadf882d5503e372db4f5b75
Petersil1998 <silbernagl.peter@rolmail.net>
2023-07-30 21:58:03 +0200

    added cdragon base path

--- src/main/java/net/petersil98/stcommons/constants/STConstants.java
+++ src/main/java/net/petersil98/stcommons/constants/STConstants.java
@@ -1,6 +1,7 @@
 package net.petersil98.stcommons.constants;
 
 public class STConstants {
-    public static final String DDRAGON_BASE_PATH = "https://ddragon.leagueoflegends.com/";
+    public static final String DDRAGON_BASE_PATH = "https://ddragon.leagueoflegends.com";
+    public static final String CDRAGON_BASE_PATH = "https://raw.communitydragon.org";
     public static String DDRAGON_VERSION;
 }
